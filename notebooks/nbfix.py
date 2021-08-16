5c1920b416739599970d6c932005d37dc7d45a18
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-08-17 00:07:19 +0200

    Add an intro.ipynb with the Kedro catalog loaded

--- notebooks/nbfix.py
+++ notebooks/nbfix.py
@@ -1,5 +1,11 @@
+import logging
 import os
 import sys
+from warnings import filterwarnings
+
+from kedro.framework.session import KedroSession
+from kedro.framework.session.session import _activate_session
+from kedro.framework.startup import _get_project_metadata
 
 
 def add_import_path():
@@ -26,6 +32,36 @@ def fix_root_path():
         os.chdir(os.path.join(os.getcwd(), ".."))
 
 
+def filter_warnings():
+    """
+    Do not show me some odd warnings about `should_run_async` in notebooks
+    """
+    filterwarnings("ignore", ".*`should_run_async`.*")
+
+
+def load_catalog():
+    """
+    Loads the kedro project and returns the catalog objects
+    """
+
+    # assume that the project is in the current directory
+    project_path = os.path.abspath(".")
+
+    metadata = _get_project_metadata(project_path)
+    session = KedroSession.create(metadata.package_name, project_path)
+    _activate_session(session, force=True)
+
+    context = session.load_context()
+    catalog = context.catalog
+
+    logging.info("Loaded Kedro project %s", str(metadata.project_name))
+
+    return catalog
+
+
 # fix the path immediately upon import
 add_import_path()
 fix_root_path()
+
+# filter some warnings in notebooks
+filter_warnings()
