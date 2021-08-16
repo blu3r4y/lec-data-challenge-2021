e85a2e76d3a850ad30e8247ce91eb4ff671334f5
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-08-17 00:08:02 +0200

    Compute velocity by integrating over the acceleration sensor values

--- src/ldc2021/pipelines/preprocessing/nodes.py
+++ src/ldc2021/pipelines/preprocessing/nodes.py
@@ -1,9 +1,25 @@
+import numpy as np
 import pandas as pd
+from scipy.integrate import cumtrapz
 
 
 def transform_cycle(df: pd.DataFrame) -> pd.DataFrame:
     # remove prefix (either "train" or "test")
     prefix_length = len(df["cycle"].iloc[0].split("0")[0])
     df["cycle"] = df["cycle"].map(lambda e: int(e[prefix_length:]))
+
+    return df
+
+
+def compute_velocity(df: pd.DataFrame) -> pd.DataFrame:
+    def _compute_velocity_per_cycle(grp: pd.DataFrame):
+        vel = cumtrapz(grp["acc"], grp["phi"])
+        vel = np.insert(vel, 0, 0)
+
+        grp["vel"] = vel
+        return grp
+
+    # compute velocity by integrating acceleration per cycle
+    df = df.groupby("cycle").apply(_compute_velocity_per_cycle)
 
     return df
--- src/ldc2021/pipelines/preprocessing/pipeline.py
+++ src/ldc2021/pipelines/preprocessing/pipeline.py
@@ -8,7 +8,9 @@ def create_pipeline():
         [
             # train
             node(transform_cycle, ["ldc_train"], "__ldc_train_1"),
+            node(compute_velocity, ["__ldc_train_1"], "ldc_train_clean"),
             # test
             node(transform_cycle, ["ldc_test"], "__ldc_test_1"),
+            node(compute_velocity, ["__ldc_test_1"], "ldc_test_clean"),
         ]
     )
